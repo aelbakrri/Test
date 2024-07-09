fd94883eeaaaabe13042944b95ef4711c61bcbbc
Abdul <aelbakrri@Abduls-MacBook-Pro.local>
2024-07-09 11:09:29 -0400

    first commit

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    zentral = {
+      source = "zentralopensource/zentral"
+    }
+  }
+}
+
+provider "zentral" {
+  base_url = "https://psumac24-maryland.zentral.cloud/api/"
+  token = "Vic30FUBYuuRJgOsfgB2AGgj9gEuK4rEx7NNntGvldU8Rw31bwD2dXlT1Om3pMXc"
+}
+
+resource "zentral_tag" "my-tag" {
+  name = "My Tag"
+}
